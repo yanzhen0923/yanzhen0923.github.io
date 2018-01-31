e7a61fe46cbf8f71d188fc1b9f3d47fc5ecaf9c8
yanzhen0923 <yanzhen0923@gmail.com>
2018-01-31 21:30:44 +0100

    cssa2018galaraffle

---
+++ extra/cssa-2018-gala-raffle-batch20/raffle.js
@@ -0,0 +1,49 @@
+var max_num_winners = 20;
+var cur_num_winners = 0;
+
+var max_num_intervals = 50;
+var cur_num_intervals = 0;
+
+min = 0;
+max = 640;
+
+winner_set = new Set();
+
+function getRandomInt() {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function raffle(){
+  if(cur_num_winners < max_num_winners){
+    setTimeout(raffle, 50);
+    cur_num_intervals += 1;
+  }
+
+  for(i = cur_num_winners; i < max_num_winners;  i++){
+    var rdint = getRandomInt()
+    $("#" + i.toString()).text(rdint.toString().padStart(3, "0"));
+  }
+
+  if (cur_num_intervals > max_num_intervals){
+    for(;;){
+      var rdint = getRandomInt();
+      if(!winner_set.has(rdint)){
+        $("#" + cur_num_winners.toString()).text(rdint.toString().padStart(3, "0"));
+        console.log(rdint);
+        winner_set.add(rdint);
+        break;
+      }
+    }
+
+    cur_num_intervals = 0;
+    cur_num_winners += 1;
+
+  }
+}
+
+$(document).ready(function () {
+
+  $("#startButton").click(function() {
+    raffle();
+  });
+});
